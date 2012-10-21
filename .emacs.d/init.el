c4c0df7c098c1b2d2e66be7d63c64963162e0388
Matthew Phillips <matthew@phillipsoft.biz>
2012-10-20 21:41:09 -0400

    Emacs things.

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-to-list 'load-path "~/.emacs.d")
+(load "sibilant-mode.el")
